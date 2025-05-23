e828c3630b95873a4ac9e4c8f8f96428c182ea9a
ahmad-khan-08 <ahmadmudassir808@gmail.com>
2025-05-23 18:42:38 +0530

    Complete Files

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import ScrollToTop from './components/ScrollToTop.jsx';
+
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Router>
+      <ScrollToTop/>
+     <App />
+    </Router>
+  </StrictMode>,
+)
